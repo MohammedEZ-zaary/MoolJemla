88084c77a945877bdb37086a0ffb2ed7259180bd
MohammedEZ-zaary <mohamedzaariy988@gmail.com>
2026-03-14 16:19:10 +0000

    edit vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite' // Add this
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // This exposes the project on your local network
+    port: 5173      // Optional: specify a port
+  },
   plugins: [
     vue(),
     tailwindcss(), // Add this here
